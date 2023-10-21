6bc179c133c90168de88cc02d57cf3843745b6e3
maximus4000 <144921384+maximus4000@users.noreply.github.com>
2023-10-21 11:50:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import random
+import os
+
 import discord
 from discord.ext import commands
 
@@ -24,14 +27,33 @@ async def time_bust(ctx, material):
 
 @bot.command()
 async def foto_trash(ctx):
-    await ctx.send("фото слуцайной свалки")
+    files = os.listdir("images")
+    rand_mem = random.choice(files)
+    with open(f"images/{rand_mem}", 'rb') as file:
+        picture = discord.File(file)
+    await ctx.send(file=picture)
 
 @bot.command()
 async def sort_trash(ctx):
-    await ctx.send("Информация о сортировке мусора")
+    await ctx.send("Раздельный сбор мусора позволяет отделить перерабатываемые отходы от неперерабатываемых")
+    await ctx.send("а также выделить отдельные типы отходов, пригодные для вторичного использования.")
+    await ctx.send("отходы разделяются на:")
+    await ctx.send("бумагу")
+    await ctx.send("стекла")
+    await ctx.send("пластик")
+    await ctx.send("и пищевые отходы")
 
 @bot.command()
 async def dang_trash(ctx, material):
-    await ctx.send(f"опастности от {material}")
+    if material == "градусник":
+        await ctx.send(f"{material} чрезвычайно опасен. Он оказывают наибольший вред окружающей среде и здоровью людей")
+    elif material == "батарейка":
+        await ctx.send(f"{material} отнесена ко 2-му классу опасности, приводит к серьезным нарушениям окружающей среды")
+    elif material == "телефон":
+        await ctx.send(f"{material} ведет к нарушению экологической системы, но  через 10 лет после изъятия окружающая среда полностью восстанавливается.")
+    elif material == "пластмасса":
+        await ctx.send(f"{material} приводит к нарушениям экосистемы, однако урон относительно невелик")
+    elif material == "кирпич":
+        await ctx.send(f"{material} безопасный для экосистемы мусор, который самостоятельно разлагается естественным путем")
 
-bot.run("MTE1NTQ0MzI4ODA5NTA4MDQ3OA.GIaCys.mveuO554YmPx6gVOtOu9JEKmUXW81e9qcdYqTk")
+bot.run("MTE1NTQ0MzI4ODA5NTA4MDQ3OA.Gds0n1.8imlzaMx54iGsLRbLsdF7BjzoRguF6ymd-LIm4")
